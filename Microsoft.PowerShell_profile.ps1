9737e7f24c9e2ca13fc1e68f651fc8a1fe09a4d2
Jake Mason <dev.jakemason@gmail.com>
2023-01-11 18:26:28 -0500

    Start tracking Windows PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+Set-Alias vim nvim
+
+# We can't use "exit" as an alias, so we need to create
+# our own 'quit' function as a workaround
+function quit
+{
+  Invoke-command -ScriptBlock {exit}
+}
+
+Set-Alias e quit
+
+function openInExplorer
+{
+  Invoke-command -ScriptBlock {explorer .}
+}
+
+Set-Alias o openInExplorer
+Set-Alias open openInExplorer
