0ae5e66ed9bbf57516edc056f2eb0bcc371b26ad
Jake Mason <dev.jakemason@gmail.com>
2023-12-27 14:23:29 -0500

    Better ripgrep command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+# rbenv for Windows
+$env:RBENV_ROOT = "e:\tools\rbenv"
+# Not easy to download on Github?
+# Use a custom mirror!
+# $env:RBENV_USE_MIRROR = "https://abc.com/abc-<version>"
+
+& "$env:RBENV_ROOT\rbenv\bin\rbenv.ps1" init
+
+
 # Prefer Neovim over regular vim
 Set-Alias vim nvim
 
@@ -24,7 +33,12 @@ Set-Alias open openInExplorer
 
 # Alias grep to ripgrep which I _should_ always have 
 # installed on Windows. :)
-Set-Alias grep rg
+
+function do_ripgrep_search
+{
+  rg -M 250 @args; # -M 250 limits the output for a single line to 250 bytes... this prevents minified results from destroying the terminal output with hundreds of lines of junk
+}
+Set-Alias grep do_ripgrep_search
 
 Set-Alias hexdump Format-Hex
 
@@ -33,3 +47,13 @@ function cmake64Bit
   cmake -DCMAKE_GENERATOR_PLATFORM=x64 @args
 }
 Set-Alias cmake64 cmake64Bit
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
